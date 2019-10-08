fdb813a06e37e01592b24687b8d8498cab7bb79b
Kengo Nakajima <kengo.nakajima@gmail.com>
2019-10-08 17:47:59 +0900

    ini

---
+++ Assets/Dragon.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// タッチ情報。UnityEngine.TouchPhase に None の情報を追加拡張。
+/// </summary>
+public enum TouchInfo
+{
+    /// <summary>
+    /// タッチなし
+    /// </summary>
+    None = 99,
+
+    // 以下は UnityEngine.TouchPhase の値に対応
+    /// <summary>
+    /// タッチ開始
+    /// </summary>
+    Began = 0,
+    /// <summary>
+    /// タッチ移動
+    /// </summary>
+    Moved = 1,
+    /// <summary>
+    /// タッチ静止
+    /// </summary>
+    Stationary = 2,
+    /// <summary>
+    /// タッチ終了
+    /// </summary>
+    Ended = 3,
+    /// <summary>
+    /// タッチキャンセル
+    /// </summary>
+    Canceled = 4,
+}
+
+public static class AppUtil
+{
+    private static Vector3 TouchPosition = Vector3.zero;
+
+    /// <summary>
+    /// タッチ情報を取得(エディタと実機を考慮)
+    /// </summary>
+    /// <returns>タッチ情報。タッチされていない場合は null</returns>
+    public static TouchInfo GetTouch()
+    {
+        if (Application.isEditor)
+        {
+            if (Input.GetMouseButtonDown(0)) { return TouchInfo.Began; }
+            if (Input.GetMouseButton(0)) { return TouchInfo.Moved; }
+            if (Input.GetMouseButtonUp(0)) { return TouchInfo.Ended; }
+        }
+        else
+        {
+            if (Input.touchCount > 0)
+            {
+                return (TouchInfo)((int)Input.GetTouch(0).phase);
+            }
+        }
+        return TouchInfo.None;
+    }
+
+    /// <summary>
+    /// タッチポジションを取得(エディタと実機を考慮)
+    /// </summary>
+    /// <returns>タッチポジション。タッチされていない場合は (0, 0, 0)</returns>
+    public static Vector3 GetTouchPosition()
+    {
+        if (Application.isEditor)
+        {
+            TouchInfo touch = AppUtil.GetTouch();
+            if (touch != TouchInfo.None) { return Input.mousePosition; }
+        }
+        else
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                TouchPosition.x = touch.position.x;
+                TouchPosition.y = touch.position.y;
+                return TouchPosition;
+            }
+        }
+        return Vector3.zero;
+    }
+
+    /// <summary>
+    /// タッチワールドポジションを取得(エディタと実機を考慮)
+    /// </summary>
+    /// <param name='camera'>カメラ</param>
+    /// <returns>タッチワールドポジション。タッチされていない場合は (0, 0, 0)</returns>
+    public static Vector3 GetTouchWorldPosition(Camera camera)
+    {
+        return camera.ScreenToWorldPoint(GetTouchPosition());
+    }
+}
+
+
+public class Dragon : MonoBehaviour
+{
+    private Vector3 position;
+    private float width;
+    private float height;
+
+    TouchInfo state;
+
+    void Awake()
+    {
+        width = (float)Screen.width / 2.0f;
+        height = (float)Screen.height / 2.0f;
+
+        // Position used for the cube.
+        position = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    void OnGUI()
+    {
+        // Compute a fontSize based on the size of the screen width.
+        GUI.skin.label.fontSize = (int)(Screen.width / 40.0f);
+
+        GUI.Label(new Rect(20, 20, width, height * 0.25f),
+            "x = " + position.x.ToString("f2") +
+            ", y = " + position.y.ToString("f2") +
+            ", state = " + state );
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        state = AppUtil.GetTouch();
+        if (state == TouchInfo.Began || state == TouchInfo.Moved || state == TouchInfo.Ended ) {
+            // タッチ開始
+            position = AppUtil.GetTouchPosition();
+            position = Camera.main.ScreenToWorldPoint(position);
+        }
+        transform.position = new Vector3(position.x, position.y, -1); // position;
+//        transform.position = new Vector3( position.x / width -0.5f, position.y / height/2f - 0.5f, -1);
+    }
+}
